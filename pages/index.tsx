87c3ac01ccea0cd2c0cbac22d60e503123136b67
HEI <ysh@cneccc.local>
2022-01-11 15:31:11 +0800

    try move isMobile to state , not finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,38 @@ import React,{Component} from 'react';
 import { WithUserAgentProps, withUserAgent } from 'next-useragent'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/IndexDesktop.module.css'
+import mobileStyles from '../styles/IndexMobile.module.css'
 
 class IndexPage extends Component<WithUserAgentProps> 
 {
+  constructor(props)
+  {
+    super(props);
+    const { ua, useragent } = this.props;
+    this.state = {
+      isMobile: ua.isMobile
+    }
+
+  }//END constructor
+
   static async getInitialProps(ctx) 
   {
     return { useragent: ctx.ua.source }
-  }
+  }//END getInitialProps
 
   render() 
   {
     const { ua, useragent } = this.props
     return  <>
+              <Head>
+                <title>Hei Yeung:Web Developer</title>
+              </Head>
               <p>{ useragent }</p>
-              {ua.isMobile ? <div className={styles.mobile}>mobile</div> : <div className={styles.desktop}>desktop</div>}
+              <div className={this.state.isMobile ? styles.mobile:styles.desktop}>{ua.isMobile ? "mobile" : "desktop"}</div>
             </>
-  }
+  }//END render
 
-}
+}//END class IndexPage
 
 export default withUserAgent(IndexPage)
