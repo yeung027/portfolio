bd235f06d84249342d95c174cbb352c74ea81ae8
HEI <ysh@cneccc.local>
2022-01-12 11:46:36 +0800

    add introduction compontent, setup ref, parent etc

---
+++ components/introduction.tsx
@@ -0,0 +1,26 @@
+import React,{Component} from 'react';
+
+class Introduction extends Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.parent = props.parent;
+
+    this.state = {
+      
+    }//END state
+
+  }//END constructor
+
+
+  render() 
+  {
+    return  <>
+             i am Introduction
+            </>
+  }//END render
+
+}//END class IndexPage
+
+export default Introduction
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/IndexDesktop.module.css'
 import mobileStyles from '../styles/IndexMobile.module.css'
+import Introduction from '../components/introduction'
 
 class IndexPage extends Component<WithUserAgentProps> 
 {
@@ -12,9 +13,11 @@ class IndexPage extends Component<WithUserAgentProps>
     super(props);
     const { ua, useragent } = this.props;
     this.state = {
-      isMobile: ua.isMobile
-    }
+      isMobile: ua.isMobile,
+      useragent: useragent,
+    }//END state
 
+    this.introduction = React.createRef();
   }//END constructor
 
   static async getInitialProps(ctx) 
@@ -22,15 +25,20 @@ class IndexPage extends Component<WithUserAgentProps>
     return { useragent: ctx.ua.source }
   }//END getInitialProps
 
+  componentDidMount()
+  {
+    //console.log('index, componentDidMount: ' + this.introduction.current.state.hello);
+  }
+
   render() 
   {
-    const { ua, useragent } = this.props
     return  <>
               <Head>
                 <title>Hei Yeung:Web Developer</title>
               </Head>
-              <p>{ useragent }</p>
-              <div className={this.state.isMobile ? styles.mobile:styles.desktop}>{ua.isMobile ? "mobile" : "desktop"}</div>
+              <p>{ this.state.useragent }</p>
+              <div className={this.state.isMobile ? styles.mobile:styles.desktop}>{this.state.isMobile ? "mobile" : "desktop"}</div>
+              <Introduction ref={this.introduction} parent={this} />
             </>
   }//END render
 
