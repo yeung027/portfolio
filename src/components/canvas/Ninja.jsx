10634278ddcb45966438366c601abb4c898c92da
yeung027 <shinghei.yeung@gmail.com>
2022-01-31 12:54:29 +0800

    load and apply ninja texture successly #2 (prev push fotgot add --all, lol)

--- src/components/canvas/Ninja.jsx
+++ src/components/canvas/Ninja.jsx
@@ -2,8 +2,10 @@ import React, { Suspense, Component } from 'react'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { PerspectiveCamera, Preload } from '@react-three/drei'
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { Vector3 } from 'three';
+import { Vector3, MeshStandardMaterial } from 'three';
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
 
+const retryInterval = 23;
 let gltf  = null;
 
 const R3F = ({ children }) => {
@@ -21,9 +23,9 @@ const R3F = ({ children }) => {
               {children}
       </Canvas>
     )
-}
+}//END R3F
 
-class Ninja extends Component
+export default class Ninja extends Component
 {
   constructor(props)
   {
@@ -44,10 +46,10 @@ class Ninja extends Component
           </R3F>
         </>
     )
-  }
+  }//END render
   
-}
-export default Ninja
+}//END CLASS Ninja
+
 
 function Asset({ url })
 {
@@ -60,15 +62,107 @@ class Model extends Component
   constructor(props)
   {
     super(props);
+      
+    this.state = {
+      mixer: null,
+      animationPlaying: false
+    }//END state
+      
+    this.isMaterialApplied = false;
+      
+    this.sendMaterial = this.sendMaterial.bind(this);
+    this.applyMaterial = this.applyMaterial.bind(this);
+    this.applyMaterialToModel = this.applyMaterialToModel.bind(this);
   }//END constructor
+    
+  sendMaterial(material)
+  {
+    this.applyMaterial(material);
+  }//END sendMaterial
+
+    
+  applyMaterial(material)
+  {
+    if(this.isMaterialApplied) return;
+    if (gltf === null || !gltf)
+    {
+      setTimeout(
+        function () {
+          console.log('gltf not loaded yet, retry is pending...');
+          this.applyMaterial(material);
+        }
+          .bind(this),
+        retryInterval
+      );
+      return;
+    }//end gltf == null
+
+    if (!this.applyMaterialToModel(material))
+      return setTimeout(
+        function () {
+        
+          this.applyMaterial(material);
+        }
+        .bind(this),
+        retryInterval
+      );
+    this.isMaterialApplied = true;
+  }//END applyMaterial
+    
+  applyMaterialToModel(material)
+  {
+    if (gltf === null || !gltf.scene) return false;
+
+    gltf.scene.traverse((o) => {
+      if (o.isMesh) {
+        o.material = material;
+      }
+    });
+
+    return true;
+    console.log('finally.....');
+  }//END applyMaterialToModel
     
   render()
   {
       return (
           <Suspense fallback={<>Loading...</>} r3f>
+            <Texture parent={this} />
             <Asset url="/ninja/ninja.gltf" />
           </Suspense>
       )
   }//END render
     
-}
+}//END Model
+
+function Texture(props)
+{
+  const parent = props.parent;
+  let loaded = false;
+
+  const [basecolor, height, metallic, normal, roughness] = useLoader(TextureLoader, [
+    '/ninja/texture/basecolor.png',
+    '/ninja/texture/height.png',
+    '/ninja/texture/metallic.png',
+    '/ninja/texture/normal.png',
+    '/ninja/texture/roughness.png',
+  ]);
+
+  if (!loaded)
+  {
+    let minigunMaterial = new MeshStandardMaterial({
+      map: basecolor,
+      displacementMap: height,
+      metalnessMap: metallic,
+      normalMap: normal,
+      roughness
+    });
+
+  
+    parent.sendMaterial(minigunMaterial);
+    loaded = true;
+  }//END if (!loaded)
+  
+
+  return <></>
+}//END Texture
