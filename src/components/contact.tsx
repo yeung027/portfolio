a8b0f598e6291adbc8ec2caedc6fb9e73aefc1d0
HEI <ysh@cneccc.local>
2022-02-09 16:55:33 +0800

    contact css form image change to css background image

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -41,10 +41,6 @@ class Contact extends Component<MyProps, MyStates>
               <div className={this.parent.state.isMobile ? mobileStyles.formContainer : styles.formContainer}>
                 <div className={this.parent.state.isMobile ? mobileStyles.row1 : styles.row1}>
                   <div className={this.parent.state.isMobile ? mobileStyles.row1Left : styles.row1Left}>
-                    <Image src="/contact/head.png"
-                      width={this.parent.state.isMobile ? '272' : '272'} 
-                      height={this.parent.state.isMobile ? '289' : '289'} 
-                    />
                   </div>
                   <div className={this.parent.state.isMobile ? mobileStyles.row1Right : styles.row1Right}>
                     <div className={this.parent.state.isMobile ? mobileStyles.ninjaDialog : styles.ninjaDialog}>
@@ -71,45 +67,21 @@ class Contact extends Component<MyProps, MyStates>
                 </div>
                 <div className={this.parent.state.isMobile ? mobileStyles.row2 : styles.row2}>
                   <div className={this.parent.state.isMobile ? mobileStyles.row2L : styles.row2L}>
-                    <Image src="/contact/2l.png"
-                      width={this.parent.state.isMobile ? '76' : '76'} 
-                      height={this.parent.state.isMobile ? '55' : '55'} 
-                    />
                   </div>
                   <div className={this.parent.state.isMobile ? mobileStyles.row2L2 : styles.row2L2}>
-                    <Image src="/contact/2l2.png"
-                      width={this.parent.state.isMobile ? '196' : '196'} 
-                      height={this.parent.state.isMobile ? '55' : '55'} 
-                    />
                   </div>
                   <div className={this.parent.state.isMobile ? mobileStyles.row2M : styles.row2M}>
-                  <Image src="/contact/2m.png"
-                      width={this.parent.state.isMobile ? '655' : '655'} 
-                      height={this.parent.state.isMobile ? '55' : '55'} 
-                    />
                   </div>
                   <div className={this.parent.state.isMobile ? mobileStyles.row2R : styles.row2R}>
-                  <Image src="/contact/2r.png"
-                      width={this.parent.state.isMobile ? '53' : '53'} 
-                      height={this.parent.state.isMobile ? '55' : '55'} 
-                    />
                   </div>
                 </div>
                 <div className={this.parent.state.isMobile ? mobileStyles.row3 : styles.row3}>
                   <div className={this.parent.state.isMobile ? mobileStyles.row3L : styles.row3L}>
-                  <Image src="/contact/3l.png"
-                      width={this.parent.state.isMobile ? '76' : '76'} 
-                      height={this.parent.state.isMobile ? '18' : '18'} 
-                    />
                   </div>
                   <div className={this.parent.state.isMobile ? mobileStyles.row3M : styles.row3M}>
                   
                   </div>
                   <div className={this.parent.state.isMobile ? mobileStyles.row3R : styles.row3R}>
-                    <Image src="/contact/r.png"
-                      width={this.parent.state.isMobile ? '53' : '53'} 
-                      height={this.parent.state.isMobile ? '18' : '18'} 
-                    />
                   </div>
                 </div>
                 <div className={this.parent.state.isMobile ? mobileStyles.row4 : styles.row4}>
@@ -139,22 +111,10 @@ class Contact extends Component<MyProps, MyStates>
                 </div>
                 <div className={this.parent.state.isMobile ? mobileStyles.row5 : styles.row5}>
                   <div className={this.parent.state.isMobile ? mobileStyles.row5L : styles.row5L}>
-                    <Image src="/contact/5l.png"
-                      width={this.parent.state.isMobile ? '53' : '53'} 
-                      height={this.parent.state.isMobile ? '55' : '55'} 
-                    />
                   </div>
                   <div className={this.parent.state.isMobile ? mobileStyles.row5M : styles.row5M}>
-                    <Image src="/contact/5m.png"
-                      width={this.parent.state.isMobile ? '851' : '851'} 
-                      height={this.parent.state.isMobile ? '55' : '55'} 
-                    />
                   </div>
                   <div className={this.parent.state.isMobile ? mobileStyles.row5R : styles.row5R}>
-                    <Image src="/contact/5r.png"
-                      width={this.parent.state.isMobile ? '53' : '53'} 
-                      height={this.parent.state.isMobile ? '55' : '55'} 
-                    />
                   </div>
                 </div>
               </div>
