ca4e91b153e082371105f772e5e17735ee7f5f29
yeung027 <shinghei.yeung@gmail.com>
2022-03-22 17:36:51 +0800

    add ico #2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return  <>
+            <Head>
+              <link rel="shortcut icon" href="/icos/favicon.ico" />
+              <link rel="apple-touch-icon" sizes="180x180" href="/icos/apple-touch-icon.png" />
+              <link rel="icon" type="image/png" sizes="32x32" href="/icos/favicon-32x32.png"/>
+              <link rel="icon" type="image/png" sizes="16x16" href="/icos/favicon-16x16.png"/>
+            </Head>
+            <Component {...pageProps} />
+          </>
 }
 
 export default MyApp
