6a07e87e55aca8e065dc771e09cc5cc75a79bcc5
yeung027 <shinghei.yeung@gmail.com>
2023-01-03 09:18:18 +0800

    nav

--- components/top.tsx
+++ components/top.tsx
@@ -1,26 +1,42 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { useAppSelector } from '../app/hooks';
 import ninjaIcon from '../public/images/ninja-icon.png';
 import { 
     UtilState,
     utilState as originUtilState
 } from "../reducers/util";
+import content from '../jsons/nav.content.json';
+import { useRouter } from 'next/router';
 
 export default function Top()
 {
     const utilState:UtilState                   = useAppSelector(originUtilState);
-
+    const {locale}                              = useRouter();
 
     return (
         <div
             className={` fixed z-[30] w-screen h-[33px] top-[19px] desktop:top-[40px] grid grid-cols-[1fr_32px_1fr]`}
         >
           <div
-            className={`${utilState.isMobile? 'test1' : 'test2'}`}
+            className={`${utilState.isMobile? 'test1' : ''}`}
           >
-            {/* {!utilState.isMobile &&
-
-            } */}
+            {!utilState.isMobile &&
+                <nav className={`nav h-full flex flex-row`}>
+                    <Link href={'/'}>
+                        <span>{content.translations[locale].home}</span>
+                    </Link>
+                    <Link href={'/'}>
+                        <span>{content.translations[locale].skills}</span>
+                    </Link>
+                    <Link href={'/'}>
+                        <span>{content.translations[locale].portfolio}</span>
+                    </Link>
+                    <Link href={'/'}>
+                        <span>{content.translations[locale].contact}</span>
+                    </Link>
+                </nav>
+            }
           </div>
           <div
             className={`relative`}
--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+const i18n = {
+  locales: ['en_US', 'zh-hk'],
+  defaultLocale: 'en_US',
+}
+
+nextConfig.i18n = i18n;
+
 module.exports = nextConfig
